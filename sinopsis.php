8f396239ca7935e7ff039da97161a35f8a59dda9
zatulsuhaida <zatulsuhaida98@yahoo.com>
2021-09-06 13:05:42 +0800

    edit labex2

--- sinopsis.php
+++ sinopsis.php
@@ -1,47 +1,50 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <title>SINOPSIS NOVEL MR DR ALPHA</title>
 </head>
+
 <body>
-  
-<div class="header">
 
-  <h1>NOVEL MR DR ALPHA</h1></center>
-  
-</div>
+    <div class="header">
 
-<div class="navbar">
+        <h1>NOVEL MR DR ALPHA</h1>
+        </center>
 
-<?php include "containingMenu.php"; ?>
+    </div>
 
-</div> 
-<?php
-echo "<p>SINOPSIS </p>";
-?>
- <center>
- <h2>PENULIS: ANJELL
-TAJUK: DR. ALPHA MALE
-HARGA : RM27 (SM)/RM30(SS)</h2>
+    <div class="navbar">
 
-<p>Alpha Male. Kata kunci seorang alpha male adalah keyakinan! Yakin dengan rupanya. Yakin dengan gayanya. Dan dah tentulah yakin dengan percakapannya. Kalau tak, takkanlah terkeluar kata-kata ini ketika kali pertama mereka berdua berlanggar di depan sebuah kedai.
+        <?php include "containingMenu.php"; ?>
 
- "Dah habis tenung dah? Tak nak ambil gambar ke? Letak bawah bantal. Malam-malam boleh cium." - HILMAN UKASYAH 
+    </div>
+    <?php
+    echo "<p>SINOPSIS </p>";
+    ?>
+    <center>
+        <h2>PENULIS: ANJELL
+            TAJUK: DR. ALPHA MALE
+            HARGA : RM27 (SM)/RM30(SS)</h2>
 
-Wow! Speechless terus Tristin Hazel mendengarnya! Apatah lagi mulut si mamat itu menyambung. 
+        <p>Alpha Male. Kata kunci seorang alpha male adalah keyakinan! Yakin dengan rupanya. Yakin dengan gayanya. Dan dah tentulah yakin dengan percakapannya. Kalau tak, takkanlah terkeluar kata-kata ini ketika kali pertama mereka berdua berlanggar di depan sebuah kedai.
 
-"Lain kali jalan guna mata. Menyusahkan orang aje. Sebab tu Tuhan ciptakan mata kat kepala. Bukan kat lutut." - HILMAN UKASYAH 
-Kena pula si lawannya Hazel, si kaki menjawab kelas pertama. Memang taklah dia nak duduk diam! Apatah lagi semuanya bukan salahnya. 
+            "Dah habis tenung dah? Tak nak ambil gambar ke? Letak bawah bantal. Malam-malam boleh cium." - HILMAN UKASYAH
 
-"Cakap elok-elok sikit tak boleh ke, sedara?" - TRISTIN HAZEL
-Dan beberapa hari selepas itu, rasa macam nak kena serangan jantung Hazel apabila orang sama yang dilanggar itu tiba-tiba buat grand entrance di tengah-tengah dewan kuliah. Ya... Dengan rasminya, alpha male tu dah bertukar jadi pensyarahnya. 
+            Wow! Speechless terus Tristin Hazel mendengarnya! Apatah lagi mulut si mamat itu menyambung.
 
-Permulaan perkenalan mereka tak indah langsung. Kena maki, dah. Menangis, pun dah. Hubungan pun tak lebih daripada pensyarah-pelajar. Dr. Alpha Male barbarian bak kata Hazel. Sehinggalah satu malam yang membawa sisi protective daripada si Dr. Alpha Male itu untuk Hazel. Hilman selalu ada untuk memberi Hazel semangat. 
+            "Lain kali jalan guna mata. Menyusahkan orang aje. Sebab tu Tuhan ciptakan mata kat kepala. Bukan kat lutut." - HILMAN UKASYAH
+            Kena pula si lawannya Hazel, si kaki menjawab kelas pertama. Memang taklah dia nak duduk diam! Apatah lagi semuanya bukan salahnya.
 
-Rupa-rupanya apa yang jadi itu membawa kepada pembongkaran kisah yang baru. Malam itu cumalah satu permulaan! Dan... Setelah itu Hazel selalu menjadi Damsel in Distress dan Hilman pula tak putus-putus menjadi Knight in Shining Armornya. Kerana janji Hilman padanya...
+            "Cakap elok-elok sikit tak boleh ke, sedara?" - TRISTIN HAZEL
+            Dan beberapa hari selepas itu, rasa macam nak kena serangan jantung Hazel apabila orang sama yang dilanggar itu tiba-tiba buat grand entrance di tengah-tengah dewan kuliah. Ya... Dengan rasminya, alpha male tu dah bertukar jadi pensyarahnya.
 
-"I'll do my best to protect you. Anything. I'll even be taking a bullet for you." - HILMAN UKASYAH 
-Namun, mampukah segalanya terbongkar? Adakah semuanya memang dirancang? Atau sebenarnya hanyalah helah untuk memperdaya?</p>
-</center>
+            Permulaan perkenalan mereka tak indah langsung. Kena maki, dah. Menangis, pun dah. Hubungan pun tak lebih daripada pensyarah-pelajar. Dr. Alpha Male barbarian bak kata Hazel. Sehinggalah satu malam yang membawa sisi protective daripada si Dr. Alpha Male itu untuk Hazel. Hilman selalu ada untuk memberi Hazel semangat.
+
+            Rupa-rupanya apa yang jadi itu membawa kepada pembongkaran kisah yang baru. Malam itu cumalah satu permulaan! Dan... Setelah itu Hazel selalu menjadi Damsel in Distress dan Hilman pula tak putus-putus menjadi Knight in Shining Armornya. Kerana janji Hilman padanya...
+
+            "I'll do my best to protect you. Anything. I'll even be taking a bullet for you." - HILMAN UKASYAH
+            Namun, mampukah segalanya terbongkar? Adakah semuanya memang dirancang? Atau sebenarnya hanyalah helah untuk memperdaya?</p>
+    </center>
 </body>
 </html>
--- troleybook.php
+++ troleybook.php
@@ -1,104 +1,102 @@
 <!DOCTYPE html>
 <html>
+
 <head>
-        <link href="styles.css" rel="stylesheet">
+    <link href="styles.css" rel="stylesheet">
 </head>
 
 <body>
 
-<div class="header">
-  
-  <h1>Novel Mr Dr Alpha<h1></center>
-</div>
+    <div class="header">
 
-<div class="navbar">
-<?php include "containingMenu.php"; ?>
-  
-</div>
-<?php
-echo "<p>TROLEY BOOK </p>";
-?>
+        <h1>Novel Mr Dr Alpha<h1>
+                </center>
+    </div>
 
-<center>
-	<form method="post" action="">
-		<table border="0">
-		<tr>
-			<th colspan="2"><h2></h2></th>
-		</tr>
-		<tr>
-			<th>NAME:</th>
-				<td><input type="text" name="name"></td>
-		</tr>
-		
-		<tr>
-			<th> ADRESS :</th>
-				<td><textarea name="address" rows="5" cols="40"></textarea> </td>
-		</tr>
+    <div class="navbar">
+        <?php include "containingMenu.php"; ?>
 
-		<tr>
-			<th> NUMBER PHONE :</th>
-				<td><input type="number" name="contact"></td>
-		</tr>
+    </div>
+    <?php
+    echo "<p>TROLEY BOOK </p>";
+    ?>
 
-		<tr>
-			<th>DATE :</th>
-				<td><input type="text" name="date"></td>
-		</tr>
-		
-		<tr>
-			<th>BOOK:</th>
-				<td><select name="book" id="book"> 
-				<option value="">Choose Your Book</option>
-                <option value="DR. ALPHA MALE">DR. ALPHA MALE</option>
-				</select></td>
-		</tr>
-		
-		<tr>
-			<td colspan="2" align="center">
-			<input type="submit" name="update" value="Submit"/>
-			</td>
-		</tr>
+    <center>
+        <form method="post" action="">
+            <table border="0">
+                <tr>
+                    <th colspan="2">
+                        <h2></h2>
+                    </th>
+                </tr>
+                <tr>
+                    <th>NAME:</th>
+                    <td><input type="text" name="name"></td>
+                </tr>
 
-        <?php
+                <tr>
+                    <th> ADRESS :</th>
+                    <td><textarea name="address" rows="5" cols="40"></textarea> </td>
+                </tr>
 
-if(isset($_POST['update'])){
-	$name=$_POST['name'];
-	$address=$_POST['address'];
-	$contact=$_POST['contact'];
-	$Date=$_POST['date'];
-	$Book=$_POST['book'];
-	
+                <tr>
+                    <th> NUMBER PHONE :</th>
+                    <td><input type="number" name="contact"></td>
+                </tr>
 
- if($name==""){
-	echo "<script>alert('Name is required');</script>";
-}
- else if($address==""){
-	echo "<script>alert('Address is required');</script>";
-}
- else if($contact==""){
-	echo"<script>alert('Contact No is required');</script>";
-}
+                <tr>
+                    <th>DATE :</th>
+                    <td><input type="text" name="date"></td>
+                </tr>
+
+                <tr>
+                    <th>BOOK:</th>
+                    <td><select name="book" id="book">
+                            <option value="">Choose Your Book</option>
+                            <option value="DR. ALPHA MALE">DR. ALPHA MALE</option>
+                        </select></td>
+                </tr>
+
+                <tr>
+                    <td colspan="2" align="center">
+                        <input type="submit" name="update" value="Submit" />
+                    </td>
+                </tr>
+
+                <?php
+
+                if (isset($_POST['update'])) {
+                    $name = $_POST['name'];
+                    $address = $_POST['address'];
+                    $contact = $_POST['contact'];
+                    $Date = $_POST['date'];
+                    $Book = $_POST['book'];
 
 
-else{
-	session_start();
+                    if ($name == "") {
+                        echo "<script>alert('Name is required');</script>";
+                    } else if ($address == "") {
+                        echo "<script>alert('Address is required');</script>";
+                    } else if ($contact == "") {
+                        echo "<script>alert('Contact No is required');</script>";
+                    } else {
+                        session_start();
 
-	$_SESSION['name']=$_POST['name'];
-	$_SESSION['address']=$_POST['address'];
-	$_SESSION['contact']=$_POST['contact'];
-	$_SESSION['date']=$_POST['date'];
-	$_SESSION['book']=$_POST['book'];
-	
-?>
+                        $_SESSION['name'] = $_POST['name'];
+                        $_SESSION['address'] = $_POST['address'];
+                        $_SESSION['contact'] = $_POST['contact'];
+                        $_SESSION['date'] = $_POST['date'];
+                        $_SESSION['book'] = $_POST['book'];
 
-<?php
-	}
-	
-}
+                ?>
 
-?>
+                <?php
+                    }
+                }
 
-</table>
-</form>
+                ?>
+
+            </table>
+        </form>
 </body>
 </html>
